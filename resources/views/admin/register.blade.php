6228b06ba779009d9587b31d14db766cddd200b8
Rafi <rafif0278@gmail.com>
2025-07-09 14:22:22 +0700

    baruu

--- resources/views/admin/register.blade.php
+++ resources/views/admin/register.blade.php
@@ -5,7 +5,7 @@
             @method('POST')
             <div class=" p-4 sm:p-8 w-full space-y-4 sm:space-y-6">
                 <input type="text" name="code" placeholder="Enter Code..." value="{{old('code')}}" class=" w-full text-sm sm:text-base font-normal rounded-md border border-main focus:ring-third focus:border-third bg-background">
-                <button class=" text-sm sm:text-base w-full py-2 border border-neutral-600 text-neutral-600 hover:border-main hover:bg-main hover:text-white duration-300 font-semibold rounded-md">Save</button>
+                <button class=" text-sm sm:text-base w-full py-2 border border-neutral-600 text-neutral-600 hover:border-main hover:bg-main hover:text-white duration-300 font-semibold rounded-md">Simpan</button>
             </div>
         </form>
     </div>
